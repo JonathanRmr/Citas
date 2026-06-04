59a2a05a0fef0683e7c8f605928e42ac55dccab1
Alejandro Huerfano <alejandro.huerfano@uptc.edu.co>
2026-06-04 14:43:35 -0500

    creacion del swagger

--- src/app.js
+++ src/app.js
@@ -17,6 +17,13 @@ app.get('/api/salud', (req, res) => {
 // Rutas del módulo
 app.use('/api/citas', citaRoutes);
 
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const YAML = require('js-yaml');
+
+const swaggerDoc = YAML.load(fs.readFileSync('./src/docs/swagger.yaml', 'utf8'));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
 // Ruta no encontrada (404)
 app.use((req, res) => {
   res.status(404).json({ ok: false, mensaje: 'Ruta no encontrada' });
